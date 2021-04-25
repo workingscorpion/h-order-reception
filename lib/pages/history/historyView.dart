24cf17f9b45e88dc04f1c154cda407293b6b2b7c
workingscorpion <workingscorpion@globalorder.co.kr>
2021-04-25 18:58:04 +0900

    feat: historyView

--- lib/pages/history/historyView.dart
+++ lib/pages/history/historyView.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:date_picker_timeline/date_picker_timeline.dart';
 import 'package:h_order_reception/constants/customColors.dart';
+import 'package:h_order_reception/model/historyModel.dart';
+import 'package:h_order_reception/model/menuModel.dart';
 import 'package:h_order_reception/model/orderModel.dart';
 import 'package:h_order_reception/utils/orderStatusHelper.dart';
 import 'package:intl/intl.dart';
@@ -14,23 +16,211 @@ class HistoryView extends StatefulWidget {
 }
 
 class _HistoryViewState extends State<HistoryView> {
+  List<MenuModel> menus;
+  List<HistoryModel> histories;
   List<OrderModel> _histories;
-  DatePickerController _controller = DatePickerController();
+  List<OrderModel> _selectedHistories;
+
   DateTime _selectedValue;
   DateTime _selectedYear;
+  DatePickerController _controller = DatePickerController();
+
   List<int> _flex = [2, 2, 2, 1, 2, 1];
 
   @override
   void initState() {
-    // TODO: implement initState
-    _selectedValue = DateTime.now();
-    _selectedYear = DateTime(DateTime.now().year, 1, 1);
-    _histories = [];
+    _selectToday();
+
+    menus = [
+      MenuModel(
+        boundaryId: '11',
+        count: 1,
+        name: '아메리카노',
+        objectId: '111',
+        price: 1000,
+      ),
+      MenuModel(
+        boundaryId: '11',
+        count: 2,
+        name: '에스프레소',
+        objectId: '222',
+        price: 3500,
+      ),
+    ];
+
+    histories = [
+      HistoryModel(
+        objectId: '55555',
+        orderObjectId: '1',
+        status: 4,
+        updatedDate: DateTime.now().subtract(Duration(minutes: 5)),
+        updaterName: '준기',
+      ),
+      HistoryModel(
+        objectId: '44444',
+        orderObjectId: '1',
+        status: 3,
+        updatedDate: DateTime.now().subtract(Duration(minutes: 10)),
+        updaterName: '준기',
+      ),
+      HistoryModel(
+        objectId: '33333',
+        orderObjectId: '1',
+        status: 2,
+        updatedDate: DateTime.now().subtract(Duration(minutes: 30)),
+        updaterName: '준기',
+      ),
+      HistoryModel(
+        objectId: '22222',
+        orderObjectId: '1',
+        status: 1,
+        updatedDate: DateTime.now().subtract(Duration(hours: 1)),
+        updaterName: '준기',
+      ),
+      HistoryModel(
+        objectId: '11111',
+        orderObjectId: '1',
+        status: 0,
+        updatedDate: DateTime.now().subtract(Duration(hours: 2)),
+        updaterName: '준기',
+      ),
+    ];
+
+    _histories = [
+      OrderModel(
+        objectId: '1',
+        status: 0,
+        applyTime: DateTime.now().subtract(Duration(days: 2)),
+        roomNumber: '1208',
+        shopName: '던킨 도넛',
+        address: '마곡럭스나인오피스텔 L동',
+        menus: [
+          MenuModel(
+            boundaryId: '11',
+            count: 1,
+            name: '도넛',
+            objectId: '111',
+            price: 1000,
+          ),
+        ],
+        histories: [...histories],
+      ),
+      OrderModel(
+        objectId: '2',
+        status: 1,
+        applyTime: DateTime.now()
+            .subtract(Duration(days: 1))
+            .subtract(Duration(hours: 6)),
+        roomNumber: '1208',
+        shopName: '고샵',
+        address: '마곡럭스나인오피스텔 L동',
+        menus: [
+          MenuModel(
+            boundaryId: '11',
+            count: 1,
+            name: '샴푸',
+            objectId: '111',
+            price: 1000,
+          ),
+        ],
+        histories: [...histories],
+      ),
+      OrderModel(
+        objectId: '3',
+        status: 2,
+        applyTime: DateTime.now()
+            .subtract(Duration(days: 1))
+            .subtract(Duration(hours: 3)),
+        roomNumber: '1208',
+        shopName: '웨스트도어',
+        address: '마곡럭스나인오피스텔 L동',
+        menus: [
+          MenuModel(
+            boundaryId: '11',
+            count: 1,
+            name: '아메리카노',
+            objectId: '111',
+            price: 1000,
+          ),
+        ],
+        histories: [...histories],
+      ),
+      OrderModel(
+        objectId: '4',
+        status: 3,
+        applyTime: DateTime.now()
+            .subtract(Duration(days: 1))
+            .subtract(Duration(hours: 1)),
+        roomNumber: '1208',
+        shopName: '봉보야쥬',
+        address: '마곡럭스나인오피스텔 L동',
+        menus: [
+          MenuModel(
+            boundaryId: '11',
+            count: 1,
+            name: '파스타',
+            objectId: '111',
+            price: 1000,
+          ),
+        ],
+        histories: [...histories],
+      ),
+      OrderModel(
+        objectId: '5',
+        status: 4,
+        applyTime: DateTime.now().subtract(Duration(hours: 3)),
+        roomNumber: '1208',
+        shopName: '비베러디시',
+        address: '마곡럭스나인오피스텔 L동',
+        menus: [
+          MenuModel(
+            boundaryId: '11',
+            count: 1,
+            name: '자몽주스',
+            objectId: '111',
+            price: 1000,
+          ),
+        ],
+        histories: [...histories],
+      ),
+      OrderModel(
+        objectId: '6',
+        status: 4,
+        applyTime: DateTime.now().subtract(Duration(minutes: 10)),
+        roomNumber: '1208',
+        shopName: '맛집',
+        address: '마곡럭스나인오피스텔 L동',
+        menus: [
+          MenuModel(
+            boundaryId: '11',
+            count: 1,
+            name: '맛있는 음식',
+            objectId: '111',
+            price: 1000,
+          ),
+        ],
+        histories: [...histories],
+      ),
+    ];
+
+    _selectedHistories =
+        _histories.where((element) => _compare(element.applyTime)).toList();
+    _historySort();
     super.initState();
 
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
       _moveDatePicker();
     });
+  }
+
+  _historySort() {
+    _selectedHistories
+        .sort((a, b) => a.applyTime.isAfter(b.applyTime) ? -1 : 1);
+  }
+
+  _compare(DateTime applyTime) {
+    final Duration diff = applyTime.difference(_selectedValue);
+    return diff.inHours < 24 && diff.inHours >= 0 ? true : false;
   }
 
   _moveDatePicker() {
@@ -39,6 +229,18 @@ class _HistoryViewState extends State<HistoryView> {
       duration: Duration(milliseconds: 300),
       curve: Curves.easeIn,
     );
+    _selectedHistories =
+        _histories.where((element) => _compare(element.applyTime)).toList();
+    _historySort();
+
+    setState(() {});
+  }
+
+  _selectToday() {
+    _selectedYear =
+        DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day);
+    _selectedValue =
+        DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day);
   }
 
   @override
@@ -49,6 +251,7 @@ class _HistoryViewState extends State<HistoryView> {
         children: <Widget>[
           _yearPicker(context),
           _datePicker(),
+          _row(),
           _rows(),
         ],
       ),
@@ -58,7 +261,11 @@ class _HistoryViewState extends State<HistoryView> {
   _rows() => Expanded(
         child: ListView(
           children: List.generate(
-              _histories.length, (index) => _row(item: _histories[index])),
+            _selectedHistories.length,
+            (index) => _row(
+              item: _selectedHistories.toList()[index],
+            ),
+          ),
         ),
       );
 
@@ -67,16 +274,33 @@ class _HistoryViewState extends State<HistoryView> {
   }) =>
       Container(
         padding: EdgeInsets.symmetric(
-          vertical: 16,
+          vertical: 10,
           horizontal: 26,
         ),
+        decoration: item != null
+            ? BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(
+                    color: CustomColors.tableInnerBorder,
+                    width: 1,
+                  ),
+                ),
+              )
+            : BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(
+                    color: CustomColors.tableOuterBorder,
+                    width: 1,
+                  ),
+                ),
+              ),
         child: Row(
           children: List.generate(
               _flex.length,
               (index) => Expanded(
                     flex: _flex[index],
                     child: Container(
-                      alignment: Alignment.centerLeft,
+                      alignment: Alignment.center,
                       child: item != null
                           ? _rowItem(item, index)
                           : _rowTitle(index),
@@ -88,25 +312,28 @@ class _HistoryViewState extends State<HistoryView> {
   Widget _rowItem(OrderModel item, int index) {
     switch (index) {
       case 0:
-        return Text(DateFormat("yyyy/MM/dd hh:mm:ss")
-            .format(item.applyTime)
-            .toString());
+        return Text(
+          DateFormat("yyyy/MM/dd HH:mm:ss").format(item.applyTime).toString(),
+        );
       case 1:
         return Column(
           children: [
             Text(
               item.address,
+              style: TextStyle(fontSize: 13),
+            ),
+            Text(
+              item.roomNumber,
               style: TextStyle(fontSize: 16),
             ),
-            Text(item.roomNumber)
           ],
         );
       case 2:
         return Text(item.menus.first.name);
       case 3:
-        return Text('총 ${item.menus.length}');
+        return Text('총 ${item.menus.length}개');
       case 4:
-        return Text('총 ${NumberFormat().format(item.menus.first.price)}');
+        return Text('총 ${NumberFormat().format(item.menus.first.price)}원');
       case 5:
         return Text(
           OrderStatusHelper.statusText[item.status],
@@ -114,6 +341,8 @@ class _HistoryViewState extends State<HistoryView> {
             color: OrderStatusHelper.statusColor[item.status],
           ),
         );
+      default:
+        return Container();
     }
   }
 
@@ -126,9 +355,12 @@ class _HistoryViewState extends State<HistoryView> {
           children: [
             Text(
               '건물명',
+              style: TextStyle(fontSize: 13),
+            ),
+            Text(
+              '방번호',
               style: TextStyle(fontSize: 16),
             ),
-            Text('방번호')
           ],
         );
       case 2:
@@ -139,6 +371,8 @@ class _HistoryViewState extends State<HistoryView> {
         return Text('가격');
       case 5:
         return Text('상태');
+      default:
+        return Container();
     }
   }
 
@@ -153,17 +387,16 @@ class _HistoryViewState extends State<HistoryView> {
         ),
         child: DatePicker(
           DateTime(_selectedYear.year, 1, 1),
-          // daysCount: 365,
+          daysCount: 365,
           initialSelectedDate: _selectedValue,
-
-          selectionColor: CustomColors.tableInnerBorder.withOpacity(.7),
+          selectionColor: CustomColors.tableInnerBorder.withOpacity(.5),
           selectedTextColor: CustomColors.aBlack,
           controller: _controller,
           locale: 'ko',
           height: 90,
           dayTextStyle: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-          onDateChange: (date) {
-            _selectedValue = date;
+          onDateChange: (DateTime date) {
+            _selectedValue = DateTime(_selectedYear.year, date.month, date.day);
             setState(() {});
             _moveDatePicker();
           },
@@ -171,15 +404,48 @@ class _HistoryViewState extends State<HistoryView> {
       );
 
   _yearPicker(BuildContext context) => Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 5,
+        ),
         alignment: Alignment.centerLeft,
         child: InkWell(
           onTap: () => showCupertinoModalPopup(
             context: context,
             builder: (BuildContext context) => Container(
-              child: Text('cndsjkllfasd'),
+              height: 500,
+              child: CupertinoPicker(
+                children: List.generate(50, (index) => _pickerItem(index)),
+                scrollController: FixedExtentScrollController(
+                    initialItem: DateTime.now().year - _selectedYear.year),
+                onSelectedItemChanged: (int index) {
+                  _selectedYear = DateTime(DateTime.now().year - index,
+                      DateTime.now().month, DateTime.now().day);
+                  _selectedValue = DateTime(
+                    _selectedYear.year,
+                    _selectedValue.month,
+                    _selectedValue.day,
+                  );
+                  WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+                    _moveDatePicker();
+                  });
+                  // _moveDatePicker();
+                  setState(() {});
+                },
+                backgroundColor: Colors.white,
+                itemExtent: 50,
+                magnification: 1.2,
+              ),
             ),
           ),
-          child: Text(_selectedYear.year.toString()),
+          child: Text(
+            _selectedYear.year.toString(),
+            style: TextStyle(fontSize: 20),
+          ),
         ),
+      );
+
+  _pickerItem(int index) => Center(
+        child: Text('${DateTime.now().year - index}'),
       );
 }
